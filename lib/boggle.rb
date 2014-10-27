2a6d8aa33546c5e9155ce1db6030d4a17b7bcb57
Layne McNish <lmcnish14@gmail.com>
2014-10-27 15:51:00 -0600

    add coordinates for board

--- lib/boggle.rb
+++ lib/boggle.rb
@@ -6,22 +6,24 @@
 class Boggle
   def initialize(board)
     @board = board
-    @coordinates = (0..15).to_a
     @dictionary = []
-    @all_possible_words = []
-    @found_words = []
-    IO.foreach("/Users/laynemcnish/gSchoolWork/boggle/bin/dictionary.txt") { |line| @dictionary << line }
+    IO.foreach("/Users/laynemcnish/gSchoolWork/boggle/bin/dictionary.txt") { |line|
+      @board.each do |letter|
+        @dictionary << line if line[0] == letter && line.length >= 3
+      end
+    }
   end
 
-  def find_all_possible_words
-    @dictionary.each do |word|
-      @board.each do |coord, letter|
-        @all_possible_words << word if word[0] == letter
-      end
-    end
+  def add_coordinates
+    @coordinates = (0..15).to_a
+    @board_hash = Hash[@coordinates.zip(@board)]
   end
+
 end
 
 board = ["a", "b", "d", "k", "e", "h", "g", "k", "s", "l", "e", "k", "g", "h", "d", "k"]
 
 Boggle.new(board)
+
+
+@found_words = []
